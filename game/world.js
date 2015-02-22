a57f4e8bab004d18f32bd8dcbefbe9c56e2c94d5
Daniel Barreto <dbarreto@talpor.com>
2015-02-21 19:46:09 -0430

    Some improvements to world.js
    Adds a `setUnit` method and defines `safeClone` as an explicit prototype method.

--- game/world.js
+++ game/world.js
@@ -4,22 +4,29 @@ var config = require('./config');
 function World(size) {
     var self = this;
 
+    this.size = size
     this.array = new Array(size);
 
     for (var i = 0; i < size; i++)
         this.array[i] = new Array(size);
-
-    this.safeClone = function () {
-        var clone = new Array(size);
-
-        for (var i = 0; i < size; i++) {
-            clone[i] = new Array(size);
-            for (var j = 0; j < size; j++)
-                if (self.array[i][j] !== null)
-                    clone[i][j] = self.array[i][j].getStats();
-        }
-    };
 }
+
+
+World.prototype.safeClone = function () {
+    var clone = new Array(this.size);
+
+    for (var i = 0; i < this.size; i++) {
+        clone[i] = new Array(this.size);
+        for (var j = 0; j < this.size; j++)
+            if (self.array[i][j] !== null)
+                clone[i][j] = self.array[i][j].getStats();
+    }
+};
+
+
+World.prototype.setUnit = function (x, y, unit) {
+    this.array[x][y] = unit;
+};
 
 
 module.exports = World;
