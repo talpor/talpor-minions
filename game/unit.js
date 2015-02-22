f7282cd787194d23002efdec7e5ee5761eb57ed1
Daniel Barreto <dbarreto@talpor.com>
2015-02-22 13:56:09 -0430

    Improvements in unit.js.
    Adds an integer autoincrement id to units instead of a uuid.

--- game/unit.js
+++ game/unit.js
@@ -5,14 +5,16 @@ var config = require('./config'),
     uuid = require('node-uuid'),
     _ = require('lodash');
 
+var pkAutoIncrement = 0;
+
 function Unit(player, hp, x, y) {
     if (!(typeof x === 'number' && typeof y === 'number' &&
-        x < config.worldSize && y < config.worldSize &&
-        x >= 0 && y >= 0)) {
+          x < config.worldSize && y < config.worldSize &&
+          x >= 0 && y >= 0)) {
         throw new Error('You must specify a valid location');
     }
     this.player = player;
-    this.id = uuid.v4();
+    this.id = pkAutoIncrement++;
     this.x = x;
     this.y = y;
     this.hp = hp;
@@ -20,6 +22,8 @@ function Unit(player, hp, x, y) {
 
 Unit.prototype.getStats = function() {
     return {
+        id: this.id,
+        player: this.player,
         x: this.x,
         y: this.y,
         hp: this.hp
@@ -40,10 +44,9 @@ AttackUnit.prototype = Object.create(Unit.prototype);
 AttackUnit.prototype.constructor = AttackUnit;
 
 AttackUnit.prototype.getStats = function () {
-    var self = this;
     return _.extend(Unit.prototype.getStats.call(this), {
-        range: self.range,
-        attack: self.attack
+        range: this.range,
+        attack: this.attack
     });
 };
 
