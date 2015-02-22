d5010a25274fa214c63d1b4836fb11259dbcc50d
Mauricio Reyes <mreyes@talpor.com>
2015-02-21 22:27:05 -0430

    add agent

--- game/agent.api.js
+++ game/agent.api.js
@@ -1,3 +1,49 @@
-var Agent = {
+var Agent = function(world) {
+    this.actions = {};
+    this.world = world;
+};
 
+Agent.prototype = {
+    constructor: Agent,
+    getMyMinons: function() {
+        // pass
+    },
+    getMyBase: function() {
+        // pass
+    },
+    getEnemyMinons: function() {
+        // pass
+    },
+    getEnemyBase: function() {
+        // pass
+    },
+    getTile: function(x, y) {
+        return this.world[x][y];
+    },
+    isUnoccupied: function(location) {
+        // pass
+    },
+    isPassable: function(location) {
+        // pass
+    },
+    getDistance: function(loc1, loc2) {
+        // pass
+    },
+    getWorld: function() {
+        return this.world;
+    },
+    walk: function(minion, x, y) {
+        this.actions[minion.id] = {
+            name: 'walk',
+            x: x,
+            y: y
+        };
+    },
+    attack: function(minion, x, y) {
+        this.actions[minion.id] = {
+            name: 'attack',
+            x: x,
+            y: y
+        };
+    }
 };
