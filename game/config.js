b1821d811ed4f75a2619ae00d208a8309b657820
Daniel Barreto <dbarreto@talpor.com>
2015-02-21 19:44:53 -0430

    Fixes config.js
    It shouldn't have an anidated 'config' key.

--- game/config.js
+++ game/config.js
@@ -1,18 +1,16 @@
 /* global module */
 
 module.exports = {
-    config: {
-        worldSize: 20,
-        maxGameTicks: 1000,
-        minon: {
-            hp: 15,
-            range: 1,
-            attack: 4
-        },
-        tower: {
-            hp: 30,
-            range: 2,
-            attack: 4
-        }
+    worldSize: 20,
+    maxGameTicks: 1000,
+    minon: {
+        hp: 15,
+        range: 1,
+        attack: 4
+    },
+    tower: {
+        hp: 30,
+        range: 2,
+        attack: 4
     }
 };
