6f2a2460b5150ce7aa96a32e43d8e484a8785810
Daniel Barreto <dbarreto@talpor.com>
2015-02-22 15:54:14 -0430

    Small frontend refactoring.

--- static/js/game.js
+++ static/js/game.js
@@ -1,156 +1,149 @@
+/* global Crafty */
+
+var NONE = 0,
+    UP = 1,
+    DOWN = 2,
+    LEFT = 3,
+    RIGHT = 4;
+
+var BOX_SIZE = 30;
+
+
 window.onload = function() {
-	//start crafty
-	BOX_SIZE = 30;
+    //start crafty
 
-
-	states = [
-		{   'x' : {
-				hp: 12,
-				player: 0,
-				x:0,
-				y:0,
-				currentAction:{
-					name: 'move',
-					x: 0,
-					y: 1,
-				}
-			},
-		 	'y' : {
-				hp: 12,
-				player: 1,
-				x:19,
-				y:19,
-				currentAction:{
-					name: 'stay',
-				}
-			}
+    var states = [
+	{
+            'x' : {
+	        hp: 12,
+	        player: 1,
+	        x:0,
+	        y:0,
+	        currentAction:{
+		    name: 'move',
+		    x: 0,
+		    y: 1
+	        }
+	    },
+	    'y' : {
+		hp: 12,
+		player: 2,
+		x:19,
+		y:19,
+		currentAction:{
+		    name: 'stay'
 		}
-		,
-		
-		{'x': {
-			hp: 12,
-			player: 0,
-			x:2,
-			y:3,
-			currentAction:{
-				name: 'attack',
-				x: 1,
-				y: 0,
-			}
-		}
-		,
+	    }
+	}
+	,
 
-		'Y': {
-			hp: 12,
-			player: 0,
-			x:19,
-			y:19,
-			currentAction:{
-				name: 'stay',
-				}
-			}
-		}
-		
-	];
+	{
+            'x': {
+	        hp: 12,
+	        player: 1,
+	        x:2,
+	        y:3,
+	        currentAction:{
+		    name: 'attack',
+		    x: 1,
+		    y: 0
+	        }
+	    }
+	    ,
 
-
-
-
-
-
-	Crafty.init(600,600, document.getElementById('game'));
-	
-	//turn the sprite map into usable components
-	Crafty.sprite(54, "/static/img/sprite.png", {
-		player1: [0,0],
-	}, 18, 18);
-
-/*	Crafty.sprite(54, "/static/img/sprite.png", {
-		player1: [0,0],
-	}, 18, 18);*/
-
-	//method to randomy generate the map
-	function generateWorld() {
-		Crafty.background("url('/static/img/bg.png')");
+	    'Y': {
+	        hp: 12,
+	        player: 2,
+	        x:19,
+	        y:19,
+	        currentAction:{
+		    name: 'stay'
+	        }
+	    }
 	}
 
-	function initMinions(state0){
-		for (var minionId in state0){
-			addMinion(minionId, 'player1', state0[minionId].x, state0[minionId].y);
-		}
-	}
-
-
-	
-	NONE = 0;
-	UP = 1;
-	DOWN = 2;
-	LEFT = 3;
-	RIGHT = 4;
-
-	//the loading screen that will display while our assets load
-	Crafty.scene("loading", function() {
-		//load takes an array of assets and a callback when complete
-		console.log('sprite loading');
-
-		Crafty.load(["/static/img/sprite.png"], function() {
-			Crafty.scene("main"); //when everything is loaded, run the main scene
-		}, function(e){
-			console.log('prog', e);
-		}
-		, function(e){
-			console.log('err', e);
-		});
-		
-		//black background with some loading text
-		Crafty.background("#000");
-		Crafty.e("2D, DOM, Text").attr({w: 100, h: 20, x: 150, y: 120})
-			.text("Loading")
-			.css({"text-align": "center"});
-	});
-	
-	//automatically play the loading scene
-	Crafty.scene("loading");
-	var left = false;
-	
-	Crafty.scene("main", function() {
-		generateWorld();
-		initMinions(states[0]);
-
-
-	});
-
-	
+    ];
 
 
 
+    Crafty.init(600,600, document.getElementById('game'));
+
+    //turn the sprite map into usable components
+    Crafty.sprite(
+        54, "/static/img/sprite.png", { 1: [0, 0], 2: [0, 0] }, 18, 18
+    );
+
+    /*	Crafty.sprite(54, "/static/img/sprite.png", {
+     player1: [0,0],
+     }, 18, 18);*/
+
+    // method to randomy generate the map
+    function generateWorld() {
+        Crafty.background("url('/static/img/bg.png')");
+    }
+
+    function initMinions(state0) {
+        var minionStats;
+        for (var minionId in state0) {
+            minionStats = state0[minionId];
+	    addMinion(minionId, minionStats.player, minionStats.x, minionStats.y);
+        }
+    }
+
+
+    //the loading screen that will display while our assets load
+    Crafty.scene("loading", function() {
+        //load takes an array of assets and a callback when complete
+        console.log('sprite loading');
+
+        Crafty.load(
+            ["/static/img/sprite.png"],
+            function () {
+	        Crafty.scene("main"); //when everything is loaded, run the main scene
+	    },
+            function (e) { console.log('prog', e); },
+            function (e) { console.log('err', e); }
+        );
+
+        // black background with some loading text
+        Crafty.background("#000");
+        Crafty.e("2D, DOM, Text").attr({w: 100, h: 20, x: 150, y: 120})
+	    .text("Loading")
+	    .css({"text-align": "center"});
+    });
+
+    //automatically play the loading scene
+    Crafty.scene("loading");
+    var left = false;
+
+    Crafty.scene("main", function() {
+        generateWorld();
+        initMinions(states[0]);
+    });
 };
 
 
+function renderAction(state) {
+    for (var minionkey in state) {
+	var minion = state[minionkey];
 
-
-	function renderAction(state){
-		for (var minionkey in state) {
-			var minion = state[minionkey];
-
-			var direction;
-			if (minion.currentAction.x == 1){
-				direction = RIGHT;
-			} else if(minion.currentAction.x == -1){
-				direction = LEFT;
-			} else if(minion.currentAction.y == 1){
-				direction = DOWN;
-			} else if(minion.currentAction.y == -1){
-				direction = UP;
-			}
-
-
-			if (minion.currentAction.name == 'move'){
-				
-				moveMinion(minions[minionkey], direction);
-			} else if (minion.currentAction.name == 'attack'){
-				//fire explosions and michael bay stuff goes here
-				attackMinion(minions[minionkey], direction);
-			}
-		}
+	var direction;
+	if (minion.currentAction.x == 1) {
+	    direction = RIGHT;
+	} else if(minion.currentAction.x == -1) {
+	    direction = LEFT;
+	} else if(minion.currentAction.y == 1) {
+	    direction = DOWN;
+	} else if(minion.currentAction.y == -1) {
+	    direction = UP;
 	}
+
+
+	if (minion.currentAction.name == 'move') {
+	    moveMinion(minions[minionkey], direction);
+	} else if (minion.currentAction.name == 'attack'){
+	    //fire explosions and michael bay stuff goes here
+	    attackMinion(minions[minionkey], direction);
+	}
+    }
+}
--- static/js/minion.js
+++ static/js/minion.js
@@ -1,76 +1,83 @@
 var minions = {};
 
 function addMinion(minionId, playerNumber, xPosition, yPosition){
-	console.log('ADding minions', arguments);
-	//create our player entity with some premade components
-	player = Crafty.e("2D, Canvas, "+playerNumber+", Controls, CustomControls, SpriteAnimation, Collision")
-		.attr({w: BOX_SIZE, h: BOX_SIZE, x: xPosition*BOX_SIZE, y: yPosition*BOX_SIZE, z: 1})
-	
-	player._step = BOX_SIZE;
-	player._moving = NONE;
-	player._moved = 0;
-	player._direction = RIGHT;
-	player._dying = false;
+    console.log('Adding minions', arguments);
 
-	player.animate("walk_left", [[2,0], [2,1], [2,2],[2,3], [2,4]])
-		.animate("walk_right", [[2,0], [2,1], [2,2],[2,3], [2,4]])
-		.animate("walk_up", [[0,0], [0,1], [0,2],[0,3], [0,4]])
-		.animate("walk_down", [[4,0], [4,1], [4,2],[4,3], [4,4]])
+    // create our player entity with some premade components
+    var player = Crafty.e("2D, Canvas, "+playerNumber+", Controls, CustomControls, SpriteAnimation, Collision")
+	    .attr({w: BOX_SIZE, h: BOX_SIZE, x: xPosition*BOX_SIZE, y: yPosition*BOX_SIZE, z: 1})
 
-		.animate("attack_up", [[0,5], [0,6], [0,7],[0,8], [0,4]])
-		.animate("attack_down", [[4,5], [4,6], [4,7],[4,8], [4,4]])
-		.animate("attack_left", [[2,5], [2,6], [2,7],[2,8], [2,4]])
-		.animate("attack_right", [[2,5], [2,6], [2,7],[2,8], [2,4]])
-		.animate("die", [[4,9], [4,10], [4,11],[3,11]])
+    player._step = BOX_SIZE;
+    player._moving = NONE;
+    player._moved = 0;
+    player._direction = RIGHT;
+    player._dying = false;
+
+    player
+        .animate("walk_left", [[2,0], [2,1], [2,2],[2,3], [2,4]])
+	.animate("walk_right", [[2,0], [2,1], [2,2],[2,3], [2,4]])
+	.animate("walk_up", [[0,0], [0,1], [0,2],[0,3], [0,4]])
+	.animate("walk_down", [[4,0], [4,1], [4,2],[4,3], [4,4]])
+
+	.animate("attack_up", [[0,5], [0,6], [0,7],[0,8], [0,4]])
+	.animate("attack_down", [[4,5], [4,6], [4,7],[4,8], [4,4]])
+	.animate("attack_left", [[2,5], [2,6], [2,7],[2,8], [2,4]])
+	.animate("attack_right", [[2,5], [2,6], [2,7],[2,8], [2,4]])
+
+	.animate("die", [[4,9], [4,10], [4,11],[3,11]])
 
 
-		.bind("EnterFrame", function(e) {
-			if (player._moving != NONE){
-				if (player._moving == UP){
-					player.animate("walk_up", 10);
-					player.y -= 1;
-				} else if (player._moving == DOWN){
-					player.animate("walk_down", 10);
-					player.y += 1;
-				} else if (player._moving == LEFT){
-					if (player._direction == RIGHT){
-						this.flip('X');	
-						player._direction = LEFT;
-					}
-					this.animate("walk_left", 10);
-					player.x -= 1;
-				} else if (player._moving == RIGHT){
-					if (player._direction == LEFT){
-						this.unflip('X');	
-						player._direction = RIGHT;
+	.bind("EnterFrame", function(e) {
 
-					}
-					this.animate("walk_right", 10);
-					player.x += 1;
-				}
-				player._moved++;
-				if (player._moved == player._step){
-					player._moving = NONE;
-					player._moved = 0;
-					player.stop();
-				}
-			} else {
-				player._moving = NONE;
-				player._moved = 0;
-			}
+            // console.log('player', player, playerNumber, 'entering frame');
 
-			if (player.dying){
-				player.alpha -= 0.01;
 
-				if (player.alpha <= 0){
-					player.destroy();
-				}
-			}
-			
-		})
-	player.id = minionId;
+	    if (player._moving != NONE) {
+		if (player._moving == UP) {
+		    player.animate("walk_up", 10);
+		    player.y -= 1;
+		} else if (player._moving == DOWN) {
+		    player.animate("walk_down", 10);
+		    player.y += 1;
+		} else if (player._moving == LEFT) {
+		    if (player._direction == RIGHT) {
+			this.flip('X');
+			player._direction = LEFT;
+		    }
+		    this.animate("walk_left", 10);
+		    player.x -= 1;
+		} else if (player._moving == RIGHT) {
+		    if (player._direction == LEFT) {
+			this.unflip('X');
+			player._direction = RIGHT;
 
-	minions[minionId] = player;
+		    }
+		    this.animate("walk_right", 10);
+		    player.x += 1;
+		}
+		player._moved++;
+		if (player._moved == player._step) {
+		    player._moving = NONE;
+		    player._moved = 0;
+		    player.stop();
+		}
+	    } else {
+		player._moving = NONE;
+		player._moved = 0;
+	    }
+
+	    if (player.dying) {
+		player.alpha -= 0.01;
+
+		if (player.alpha <= 0){
+		    player.destroy();
+		}
+	    }
+
+	})
+    player.id = minionId;
+
+    minions[minionId] = player;
 }
 
 function moveMinion(minion, direction){
@@ -80,7 +87,7 @@ function moveMinion(minion, direction){
 function attackMinion(player, direction){
 	if(direction == "LEFT") {
 		if (player._direction == RIGHT){
-			player.flip('X');	
+			player.flip('X');
 			player._direction = LEFT;
 		}
 		player.stop().animate("attack_left", 10);
@@ -88,17 +95,17 @@ function attackMinion(player, direction){
 
 	} else if(direction == "RIGHT") {
 		if (player._direction == LEFT){
-			player.unflip('X');	
+			player.unflip('X');
 			player._direction = RIGHT;
 		}
 		player.stop().animate("attack_right", 10);
 
 	} else if(direction == "UP") {
 		player.stop().animate("attack_up", 10);
-			
+
 	} else if(direction == "DOWN") {
 		player.stop().animate("attack_down", 10);
-	
+
 	}
 
 }
