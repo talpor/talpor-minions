c3e874f273f21ff405ea4142ad933138d6af2fbc
Daniel Barreto <dbarreto@talpor.com>
2015-03-03 01:05:54 -0430

    Changes gunicorn log level to warning.

--- server/gunicorn.conf.py
+++ server/gunicorn.conf.py
@@ -6,6 +6,6 @@ workers = 2
 bind = '0.0.0.0:5001'
 pidfile = os.path.join(root, 'server', 'gunicorn.pid')
 debug = True
-loglevel = 'debug'
+loglevel = 'warning'
 errorlog = os.path.join(root, 'server', 'logs', 'gunicorn.log')
 daemon = False
