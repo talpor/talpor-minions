b3d79e6875b6b2407c461e00762061bf646ae6d0
Daniel Barreto <dbarreto@talpor.com>
2015-03-02 20:43:09 -0430

    Create server/ files.

---
+++ server/gunicorn.conf.py
@@ -0,0 +1,11 @@
+import os
+
+root = os.path.split(os.path.dirname(os.path.realpath(__file__)))[0]
+
+workers = 2
+bind = '0.0.0.0:5001'
+pidfile = os.path.join(root, 'server', 'gunicorn.pid')
+debug = True
+loglevel = 'debug'
+errorlog = os.path.join(root, 'server', 'logs', 'gunicorn.log')
+daemon = False
