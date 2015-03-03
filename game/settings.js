6bb2f7f58efdf815c7f8217bb34a11b0f60b0b2a
Daniel Barreto <dbarreto@talpor.com>
2015-03-03 00:00:19 -0430

    New settings.js file.
    It'll hold different kinds of settings and it'll try to depend on an
    `NODE_ENV` variable in the ``VIKINGS_CONFIG`` env file.

---
+++ game/settings.js
@@ -0,0 +1,32 @@
+/* global process */
+
+var path = require('path'),
+    ini = require('node-ini');
+
+var _ = require('lodash');
+
+
+var Settings = {};
+
+// Common
+Settings.Common = {
+    debug: true
+};
+
+// Development
+Settings.development = {
+    debug: false,
+    sentryDSN: 'https://198259cf4480430790c2c3425a1d8f4f:a705f0505b56489abc8caf1e304eafef@sentry.talpor.com/31'
+};
+
+
+
+var vikingsConfig, extraConfiguration;
+try {
+    vikingsConfig = ini.parseSync(path.join(__dirname, process.env.VIKINGS_CONFIG));
+    extraConfiguration = Settings[vikingsConfig.NODE_ENV];
+} catch (err) {
+    extraConfiguration = {};
+};
+
+module.exports = _.merge(Settings.Common, extraConfiguration);
